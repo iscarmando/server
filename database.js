19308ce7a09a3f254d2c06acabf74bff0ec67acf
iscarmando <a.martinez.isc@gmail.com>
2021-07-02 10:43:48 -0500

    Conexion a MongoDB Atlas

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 //configuracion de los parametros de la base de datos
-url = 'mongodb://localhost/empleados';
+//url = 'mongodb://localhost/empleados';
+url = "mongodb+srv://sistema_empleados:Sistemaempleados2021@cluster0.4aivt.mongodb.net/Empleados?retryWrites=true&w=majority";
+//url="mongodb+srv://sistema_empleados:<Sistemaempleados>@cluster0.4aivt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 dbparams = {
     useCreateIndex: true,
     useNewUrlParser: true,
